706add3d04842b2bef374e5fd3002be9eeb951f1
Christophe Eymard <christophe.eymard@sales-way.com>
2017-07-03 18:56:00 +0200

    making transforms more readable

--- src/parser/type_literals.ts
+++ src/parser/type_literals.ts
@@ -5,40 +5,46 @@ import {T, K} from './base'
 import * as ast from './ast'
 
 export const 
-  argument = SequenceOf(Optional(T.ellipsis), T.id, T.colon, () => type_literal)
-    .tf(([ellipsis, id, colon, type]) => 
-      new ast.Argument()
-        .name(id.text)
-        .type(type)
-        .ellipsis(ellipsis != null)
-    ),
+  argument = SequenceOf(
+    Optional(T.ellipsis), 
+    T.id, 
+    T.colon.then(() => type_literal)
+  )
+                                  .tf(([ellipsis, id, type]) => 
+                                    new ast.Argument()
+                                      .name(id.text)
+                                      .type(type)
+                                      .ellipsis(ellipsis != null)
+                                  ),
     
   argument_list = List(argument, T.comma),
  
-  generic_arguments = SequenceOf(T.lt, List(() => type_literal, T.comma), T.gt)
-    .tf(([lt, types, gt]) => types),
+  generic_arguments = SequenceOf(
+    T.lt.then(List(() => type_literal, T.comma)), 
+    T.gt
+  )
+                                  .tf(([types]) => types),
 
   function_type_literal = SequenceOf(
     Optional(generic_arguments), 
-    T.lparen, 
-    Optional(argument_list), 
-    T.rparen,
-    T.fat_arrow,
-    () => type_literal
-  ).tf(([gen, lp, args, rp, fa, type]) => new ast.FunctionLiteral()
-    .type_arguments(gen)
-    .arguments(args)
-    .return_type(type)
-  ),
+    T.lparen.then(Optional(argument_list)), 
+    T.rparen.then(T.fat_arrow).then(() => type_literal)
+  )
+                                  .tf(([gen, args, type]) => new ast.FunctionLiteral()
+                                    .type_arguments(gen)
+                                    .arguments(args)
+                                    .return_type(type)
+                                  ),
 
   named_type = SequenceOf(
     T.id,
     Optional(generic_arguments)
-  ).tf(([id, args]) =>
-    new ast.NamedType()
-      .name(id.text)
-      .type_arguments(args)
-  ),
+  )
+                                  .tf(([id, args]) =>
+                                    new ast.NamedType()
+                                      .name(id.text)
+                                      .type_arguments(args)
+                                  ),
 
   type_literal: Rule<ast.TypeLiteral> = List(
     Either(
@@ -48,8 +54,12 @@ export const
           function_type_literal
         ),
         ZeroOrMore(SequenceOf(T.lbracket, T.rbracket))
-      ).tf(([type, array_number]) => type.array_number(array_number.length)),
-      SequenceOf(T.lparen, () => type_literal, T.rparen).tf(([lp, type, rp]) => type)
+      )
+                                  .tf(([type, array_number]) => type.array_number(array_number.length)),
+
+      SequenceOf(T.lparen, () => type_literal, T.rparen)
+                                  .tf(([lp, type, rp]) => type)
     ),
     T.pipe
-  ).tf((lst) => lst.length > 1 ? new ast.UnionType().types(lst) : lst[0])
+  )
+                                  .tf((lst) => lst.length > 1 ? new ast.UnionType().types(lst) : lst[0])
