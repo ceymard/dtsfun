7b9dd24e825f900b9c7184da8d5c6061e27f6f79
Christophe Eymard <christophe.eymard@gmail.com>
2017-07-03 11:26:56 +0200

    cosmetic changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import {
-  Token as T, _, Language, Either, List, Optional, Forward, Rule, TokenList
+  SequenceOf, Language, Either, List, Optional, Forward, Rule, TokenList, LanguageRule,
+  ZeroOrMore
 } from 'pegp'
 
 import {Type} from './ast'
@@ -11,17 +12,19 @@ import {Type} from './ast'
 const t = new TokenList()
 const sk = new TokenList()
 sk.add(/[\n\s\t\r ]+/)
+sk.add(/\/\/[^\n]*\n/) // single comment.
 
 const
-  ID = t.add(/[a-zA-Z_$][a-zA-Z0-9_$]+/),
-  LPAREN = t.add('('),
-  RPAREN = t.add(')'),
-  COLON = t.add(':'),
-  SEMICOLON = t.add(';'),
-  COMMA = t.add(','),
-  LT = t.add('<'),
-  GT = t.add('>'),
-  SINGLE_COMMENT = sk.add(/\/\/[^\n]*\n/)
+  t_id = t.add(/[a-zA-Z_$][a-zA-Z0-9_$]+/),
+  t_lparen = t.add('('),
+  t_rparen = t.add(')'),
+  t_lbracket = t.add('{'),
+  t_rbracket = t.add('}'),
+  t_colon = t.add(':'),
+  t_semicolon = t.add(';'),
+  t_comma = t.add(','),
+  t_lt = t.add('<'),
+  t_gt = t.add('>')
 
 
 ////////////////////////////////////////////
@@ -29,41 +32,53 @@ const
 const
   // since the semi colon is pretty much optional all the time,
   // make a rule that uses it as is.
-  SEMI = Optional(SEMICOLON),
+  semi = Optional(t_semicolon),
 
-  GENERICS: Rule<any> = _(LT, List(Forward(() => TYPE_DEF), COMMA), GT),
+  type_arguments: Rule<any> = SequenceOf(t_lt, List(Forward(() => type), t_comma), t_gt),
 
-  TYPE_DEF = _(
-    ID,
-    Optional(GENERICS)
+  type = SequenceOf(
+    t_id,
+    Optional(type_arguments)
   ).tf(([id, gen]) =>
     new Type()
     .name(id.text)
-    .doc('hello')
   ),
 
-  VAR_DECL = _(ID.as('const', 'var', 'let'), ID, COLON, TYPE_DEF, SEMI),
+  var_decl = SequenceOf(t_id.as('const', 'var', 'let'), t_id, t_colon, type, semi),
 
-  // Missing default value.
-  ARGUMENT = _(ID, COLON, TYPE_DEF),
+  // There are no default values in .d.ts files.
+  argument = SequenceOf(t_id, t_colon, type),
 
-  FUN_DECL = _(ID, LPAREN, Optional(List(ARGUMENT, COMMA)), RPAREN, COLON, TYPE_DEF),
+  function_decl = SequenceOf(t_id, t_lparen, Optional(List(argument, t_comma)), t_rparen, t_colon, type),
 
-  EXPORT = _(
-    ID.as('export'), Optional(ID.as('declare')), Either(
-      VAR_DECL,
-      FUN_DECL,
-      TYPE_DEF
+  export_rule = SequenceOf(
+    t_id.as('export'), Optional(t_id.as('declare')), Either(
+      var_decl,
+      function_decl,
+      type
     )
   ),
 
-  IMPORT = _(
-    ID.as('import'),
+  import_rule = SequenceOf(
+    t_id.as('import'),
+    semi
+  ),
 
-    SEMI
+  // Inline module declaration in a file of modules
+  module_decl = SequenceOf(
+    t_id.as('declare'),
+    t_id.as('module'),
+    t_id, // module name
+    t_lbracket,
+    ZeroOrMore(Either(
+      import_rule,
+      export_rule
+    )),
+    t_rbracket
   )
 
-export const TSD = Language(Either(
-  EXPORT,
-  TYPE_DEF // No.
-)).tokens(t).skip(sk)
+/**
+ * A tsd file contains either a declaration of several modules
+ * or directly exports (optionally with some sub-module declarations...)
+ */
+export const TSD: LanguageRule<any> = Language(module_decl, t)
